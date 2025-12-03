c385e9c8d6fc2cd406a229a97a9faabfc003976e
vishal <vishal.saini@icaimpetus.com>
2025-12-03 10:05:47 +0530

    login

--- broker_service.py
+++ broker_service.py
@@ -175,7 +175,8 @@ def place_Order(symbol, qty, order_type="B", price_type="MKT", limit_price="0", 
         print("Error:", str(e))
         return False
 
-# result =place_Order("RELIANCE" ,10000)
+
+# result =place_Order("RELIANCE" ,10000 , "S")
 
 # print("result ",result)
 
